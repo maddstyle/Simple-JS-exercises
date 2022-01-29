37878931d5886d54b3ed435da51e3ed6b8fad0de
Michaela DeCamara <michaela.decamara@gmail.com>
2022-01-28 23:39:00 -0500

    test

--- es6.js
+++ es6.js
@@ -176,19 +176,3 @@ const tripWithGear = new Expedition("Everest", 30, ["Jacket", "Trecking Shoes", 
 tripWithGear.info();
 
 
-// React Hooks example
-import React, { useState } from 'react';
-
-function Example() {
-  // Declare a new state variable, which we'll call "count"
-  const [count, setCount] = useState(0);
-
-  return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
-    </div>
-  );
-}
