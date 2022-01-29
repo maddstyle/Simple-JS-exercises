14660159f8905b0d9d6828512a60483a72b33543
Michaela DeCamara <michaeladecamara@Michaelas-MacBook-Pro.local>
2022-01-28 22:52:20 -0500

    hooks example

--- es6.js
+++ es6.js
@@ -174,3 +174,21 @@ class Expedition extends Holiday {
 
 const tripWithGear = new Expedition("Everest", 30, ["Jacket", "Trecking Shoes", "Camera"]);
 tripWithGear.info();
+
+
+// React Hooks example
+import React, { useState } from 'react';
+
+function Example() {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
+}
